aecdeafbd9abf2b23d814a532bfb1ababcd18641
tutruong-dev <tutruong.dev@gmail.com>
2025-06-03 22:09:05 +0700

    update entity

---
+++ Backend/FoodNest/src/main/java/com/example/foodnest/entity/DonHang.java
@@ -0,0 +1,54 @@
+package com.example.foodnest.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.Nationalized;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+
+@Getter
+@Setter
+@Entity
+public class DonHang {
+    @Id
+    @Column(name = "MaDonHang", nullable = false)
+    private String maDonHang;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "MaNguoiDung", nullable = false)
+    private NguoiDung maNguoiDung;
+
+    @ColumnDefault("getdate()")
+    @Column(name = "NgayDat")
+    private Instant ngayDat;
+
+    @Size(max = 50)
+    @Nationalized
+    @ColumnDefault("N'Chờ xác nhận'")
+    @Column(name = "TrangThaiDonHang", length = 50)
+    private String trangThaiDonHang;
+
+    @Column(name = "NgayGiaoHang")
+    private Instant ngayGiaoHang;
+
+    @Column(name = "NgayThanhToan")
+    private Instant ngayThanhToan;
+
+    @NotNull
+    @ColumnDefault("0")
+    @Column(name = "TongTien", nullable = false, precision = 18, scale = 2)
+    private BigDecimal tongTien;
+
+    @Size(max = 255)
+    @NotNull
+    @Nationalized
+    @Column(name = "DiaChiGiaoHang", nullable = false)
+    private String diaChiGiaoHang;
+
+}
--- Backend/FoodNest/src/main/java/com/example/foodnest/entity/SanPham.java
+++ Backend/FoodNest/src/main/java/com/example/foodnest/entity/SanPham.java
@@ -1,45 +1,72 @@
 package com.example.foodnest.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
-import lombok.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.Nationalized;
 
 import java.math.BigDecimal;
-import java.sql.Date;
+import java.time.Instant;
 
-@Entity
-@Table(name = "SanPham")
 @Getter
 @Setter
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
+@Entity
+
 public class SanPham {
     @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
-    @Column(name = "MaSanPham")
+    @Size(max = 50)
+    @Nationalized
+    @Column(name = "MaSanPham", nullable = false, length = 50)
     private String maSanPham;
 
-    @Column(name = "TenSanPham")
+    @Size(max = 100)
+    @NotNull
+    @Nationalized
+    @Column(name = "TenSanPham", nullable = false, length = 100)
     private String tenSanPham;
 
+    @Size(max = 255)
+    @Nationalized
     @Column(name = "MoTa")
     private String moTa;
 
-    @Column(name = "Gia")
+    @NotNull
+    @Column(name = "Gia", nullable = false, precision = 18, scale = 2)
     private BigDecimal gia;
 
+    @ColumnDefault("0")
     @Column(name = "SoLuong")
-    private int soLuong;
+    private Integer soLuong;
 
-    @Column(name = "MaGianHang")
-    private int maGianHang;
+    @NotNull
+    @JsonIgnore
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "MaGianHang", nullable = false)
+    private GianHang maGianHang;
 
+    @Size(max = 255)
+    @Nationalized
     @Column(name = "AnhChinh")
     private String anhChinh;
 
-    @Column(name = "trangThai")
-    private boolean TrangThai;
+    @ColumnDefault("1")
+    @Column(name = "TrangThai")
+    private Boolean trangThai;
 
+    @ColumnDefault("getdate()")
     @Column(name = "NgayTao")
-    private Date ngayTao;
+    private Instant ngayTao;
+
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "MaLoai")
+//    private String maLoai;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "MaLoai")
+    @JsonIgnore
+    private LoaiSanPham loaiSanPham;
 }
